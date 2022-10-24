2379a482dd30485e7e994f41d1497769490658c5
damskw <damian.skwierawski@gmail.com>
2022-10-24 19:06:59 +0200

    navbar idea

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,19 @@
 import {boardsManager} from "./controller/boardsManager.js";
+import {domManager} from "./view/domManager.js";
 
 function init() {
     boardsManager.loadBoards();
+    activateNavBar();
+}
+
+function activateNavBar() {
+    domManager.addEventListener("#user-profile-icon", "click", toggleNavBarMenu);
+}
+
+function toggleNavBarMenu() {
+    let subMenu = document.querySelector("#subMenu");
+    console.log(subMenu);
+    subMenu.classList.toggle("open-menu");
 }
 
 init();
